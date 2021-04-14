aa80a6d6c2f62f6594b5c6d92cc077dc3a10d629
Jose Antonio <joseantoniomarquezglez@gmail.com>
2021-04-14 12:36:33 +0200

    Creacion Atributos Clase Videojuego

---
+++ src/Modelo/VideoJuego.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+public class VideoJuego {
+	
+	private String titulo;
+	private String horasEstimadas;
+	private boolean entregado;
+	private String genero;
+	private String compañia;
+
+	public VideoJuego() {
+		
+	}
+
+}
