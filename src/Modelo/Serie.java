d3758ddd12897defb8557a83555586676482ac84
HoodRo.bin <aleexortega98@gmail.com>
2021-04-14 13:27:08 +0200

    hola

---
+++ src/Modelo/Serie.java
@@ -0,0 +1,16 @@
+package Modelo;
+
+public class Serie {
+	
+	private String titulo;
+	private String numTemporadas;
+	private boolean entregado;
+	private String genero;
+	private String creador;
+
+	public Serie() {
+		// TODO Auto-generated constructor stub
+		System.out.println("hola cara almeja");
+	}
+
+}
