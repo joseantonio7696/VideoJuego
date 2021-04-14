3a66dcc52ccfb6aa7508d5fc23b977752e6de73a
HoodRo.bin <aleexortega98@gmail.com>
2021-04-14 12:55:24 +0200

    hola mundo added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import Modelo.VideoJuego;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		VideoJuego videojuego=new VideoJuego();
+		System.out.println("Hola Mundo!");
 	}
 
 }
