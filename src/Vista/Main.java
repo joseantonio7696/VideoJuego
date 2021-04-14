a63cf98c7662faf09d318fd3354ee88080ecac4c
pili <pili@localhost>
2021-04-14 17:01:17 +0200

    Soy un angelito

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		VideoGame videoGame=new VideoGame();
+		
+		System.out.println("Yo no insulto!!! COJONES");
 	}
 
 }
